9174d163630856999e3a0ef62ead8bc8c9d13ed4
BrenKenna <55492113+BrenKenna@users.noreply.github.com>
2022-03-14 09:48:21 +0000

    skeleton for request forwarding

---
+++ forwarding/login/index.js
@@ -0,0 +1,5 @@
+/**
+ * Receive & process get/post requests about login
+ *  - Serve back logged-in page on success
+ *  - Needs to be aware of both the auth (get) & loggedIn (successful post)
+ */
---
+++ forwarding/main/index.js
@@ -0,0 +1,7 @@
+/**
+ * Asks the login server for the form and posts input
+ *  - Effectively get & post requests are relayed to another server
+ * 
+ * Serves messages back to the user from the login server
+ *  - That severs responses are relayed back to end-user
+ */
